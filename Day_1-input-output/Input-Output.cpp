48fe68a45d2422a816a08ff2564c087c1a2d20a2
shrey <shreyan.maity@outlook.com>
2026-06-29 12:48:39 +0530

    CPP Day 01 with some changes

--- Day_1-input-output/Input-Output.cpp
+++ Day_1-input-output/Input-Output.cpp
@@ -17,9 +17,29 @@ using namespace std;
 //     return 0;
 // }
 
+// int main(){
+//     cout << "Hello World" << "\n" ;
+//     cout << "Welcome to Programming\n";
+//     cout << "Let's learn C++" << endl;
+//     return 0;
+// }
+
+
 int main(){
-    cout << "Hello World" << "\n" ;
-    cout << "Welcome to Programming\n";
-    cout << "Let's learn C++" << endl;
+    string Name;
+    cout << "Enter your name : ";
+    // cin >> Name; //This wouldn't take surname **For "Shreyan Maity" it will only print "Shreyan"
+    getline(cin, Name); // Use getline to capture everything until you press Enter
+    cout << "Hey, " << Name << " Welcome to programming!";
+
     return 0;
 }
+
+// int main(){
+//     string colour;
+//     cout << "Enter you favorite colour : ";
+//     cin >> colour;
+//     cout << "Hey, your favorite color is " << colour << ", That's cool!" ;
+
+//     return 0;
+// }
