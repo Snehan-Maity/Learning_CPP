16c1f19ecc7642b8c631c1ebc3caea638277b524
Shreyan Maity <shreyan.maity@outlook.com>
2026-06-29 12:27:13 +0530

    Fix output statements in Input-Output.cpp

--- Day_1-input-output/Input-Output.cpp
+++ Day_1-input-output/Input-Output.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 // int main(){
 //     cout << "Hello Wrold!!!";
+//     return 0;
 // }
 
 // int main(){
 //     cout << "My name is Shreyan";
+//     return 0;
 // }
 
 // int main(){
 //     cout <<"Knowledge is power.";
+//     return 0;
 // }
 
 int main(){
     cout << "Hello World" << "\n" ;
     cout << "Welcome to Programming\n";
     cout << "Let's learn C++" << endl;
+    return 0;
 }
