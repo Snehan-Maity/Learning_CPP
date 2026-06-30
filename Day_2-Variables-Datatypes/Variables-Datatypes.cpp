e2bb10c7194df8647e9cffa3cafbb5285876db31
shrey <shreyan.maity@outlook.com>
2026-06-30 08:29:53 +0530

    CPP Day 02

--- Day_2-Variables-Datatypes/Variables-Datatypes.cpp
+++ Day_2-Variables-Datatypes/Variables-Datatypes.cpp
@@ -2,14 +2,35 @@
 
 using namespace std;
 
-int main (){
-    int age;
+// int main (){
+//     int age;
+//     string name;
+//     cout << "Enter Your Name : ";
+//     getline(cin, name);
+//     cout << "Enter your Age : ";
+//     cin >> age;
+
+//     cout << "Hello! " << name << ", You are " << age << " year old.";
+
+//     return 0;
+// }
+
+
+int main() {
     string name;
-    cout << "Enter Your Name : ";
-    getline(cin, name);
-    cout << "Enter your Age : ";
+    int age;
+    int marks;
+
+    cout << "Enter your name: ";
+    getline(cin, name); 
+
+    cout << "Enter your age: ";
     cin >> age;
 
-    cout << "Hello! " << name << ", You are " << age << " year old.";
+    cout << "Enter your marks: ";
+    cin >> marks;
 
+    cout << "Hello " << name << ", you are " << age << " years old and scored " << marks << " marks.\n";
+
+    return 0;
 }
